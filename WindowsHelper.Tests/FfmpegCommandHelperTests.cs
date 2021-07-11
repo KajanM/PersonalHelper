5cb60a34023091d0f1f523edee99835f13f74c0e
kajan <gmkajan@gmail.com>
2021-07-11 11:09:48 +0530

    test: unit test `GetMediaDurationAsync`

---
+++ WindowsHelper.Tests/FfmpegCommandHelperTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using WindowsHelper.Tasks.Helpers;
+using CliWrap;
+using CliWrap.Buffered;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace WindowsHelper.Tests
+{
+    public class FfmpegCommandHelperTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public FfmpegCommandHelperTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public async Task GetMediaDurationAsync_ShouldReturnCorrectDuration_InHappyPath()
+        {
+            var (seconds, cmdResult) = await FfmpegCommandHelper.GetMediaDurationAsync("resources/dont-worry.mp4");
+            Assert.Equal(207, seconds);
+            Assert.Empty(cmdResult.StandardError);
+        }
+    }
+}
