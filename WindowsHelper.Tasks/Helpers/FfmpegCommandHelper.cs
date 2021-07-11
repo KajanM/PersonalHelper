f03ead1abca989dbc1bdda1af7afa05eeccc752c
kajan <gmkajan@gmail.com>
2021-07-11 08:29:43 +0530

    feat: get duration of a media file

---
+++ WindowsHelper.Tasks/Helpers/FfmpegCommandHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using CliWrap;
+using CliWrap.Buffered;
+
+namespace WindowsHelper.Tasks.Helpers
+{
+    public static class FfmpegCommandHelper
+    {
+        public static async Task<(int seconds, BufferedCommandResult cmdResult)> GetMediaDurationAsync(
+            string filePathWithName)
+        {
+            var commandResult = await Cli.Wrap("ffprobe.exe")
+                .WithArguments(
+                    $"-v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 {filePathWithName}")
+                .ExecuteBufferedAsync();
+
+            var isParseableString = float.TryParse(commandResult.StandardOutput, out var seconds);
+
+            if (!isParseableString)
+                throw new ArgumentException(
+                    $"Unable to get media duration{Environment.NewLine}Seconds string: {commandResult.StandardOutput}");
+
+            var nearestSecondsInteger = (int) Math.Ceiling(seconds);
+
+            return (nearestSecondsInteger, commandResult);
+        }
+    }
+}
