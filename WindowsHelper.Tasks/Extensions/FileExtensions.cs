59d68831207995ba4ecf0cc5a79ad787fbc38fb5
kajan <gmkajan@gmail.com>
2021-07-28 17:40:50 +0530

    refactor: optimize unused import

--- WindowsHelper.Tasks/Extensions/FileExtensions.cs
+++ WindowsHelper.Tasks/Extensions/FileExtensions.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using WindowsHelper.Tasks.Helpers;
 using CliWrap.Buffered;
