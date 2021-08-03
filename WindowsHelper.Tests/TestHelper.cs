5e908570399ef2ea8e6138294a5e4274f5bf713a
kajan <gmkajan@gmail.com>
2021-08-03 07:55:27 +0530

    test: add utility method to retrieve data from appsettings in tests

---
+++ WindowsHelper.Tests/TestHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace WindowsHelper.Tests
+{
+    public static class TestHelper
+    {
+        public static TSettings GetFromAppSettings<TSettings>(string key)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false);
+
+            var rootConfig = builder.Build().GetSection("App");
+
+            return rootConfig.GetSection(key).Get<TSettings>();
+        }
+    }
+}
