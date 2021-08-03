f56a24a5e281ba3afc7964ab0c4db4b270544294
kajan <gmkajan@gmail.com>
2021-08-03 23:32:57 +0530

    feat: add `WindowsService.Shutdown()`

---
+++ WindowsHelper.Services/Windows/WindowsService.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+namespace WindowsHelper.Services.Windows
+{
+    public static class WindowsService
+    {
+        public static void Shutdown(int hours = 0)
+        {
+            Process.Start("shutdown", $"/s /t {hours * 3600}");
+        }
+    }
+}
