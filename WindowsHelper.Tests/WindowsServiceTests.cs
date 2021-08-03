0e4d3b383c5bc7a953ca88c05655e3bee685bea2
kajan <gmkajan@gmail.com>
2021-08-03 23:33:13 +0530

    test: unit test `WindowsService.Shutdown()`

---
+++ WindowsHelper.Tests/WindowsServiceTests.cs
@@ -0,0 +1,14 @@
+﻿using WindowsHelper.Services.Windows;
+using Xunit;
+
+namespace WindowsHelper.Tests
+{
+    public class WindowsServiceTests
+    {
+        [Fact]
+        public void Shutdown_ShouldShutdownTheComputer()
+        {
+          WindowsService.Shutdown(3);  
+        } 
+    }
+}
