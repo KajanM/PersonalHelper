5b1c2cfb99342dc1f3da76987b8f0abbe78fbae7
kajan <gmkajan@gmail.com>
2021-08-03 18:39:39 +0530

    fix: serialize playlist response when logging

--- WindowsHelper.Tasks/UploadToYoutube.cs
+++ WindowsHelper.Tasks/UploadToYoutube.cs
@@ -95,7 +95,7 @@ namespace WindowsHelper.Tasks
             playlistRequest.MaxResults = 1000;
             
             var playlistResponse = playlistRequest.Execute();
-            Log.Debug("Playlist list response: {0}", playlistResponse);
+            Log.Debug("Playlist list response: {@0}", playlistResponse);
             var allPlaylists = playlistResponse.Items.ToList();
             Log.Information("Received a total of {0} playlists", allPlaylists.Count);
             var playlist = allPlaylists.FirstOrDefault(playlist => playlist.Snippet.Title == title);
