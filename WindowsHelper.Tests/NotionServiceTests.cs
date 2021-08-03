079831d139600d68ea0c26004c6c8552efd0605e
kajan <gmkajan@gmail.com>
2021-08-03 07:56:17 +0530

    test: unit test `INotionService.DescribeCoursesDatabaseAsync`

---
+++ WindowsHelper.Tests/NotionServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using WindowsHelper.Services.Notion;
+using WindowsHelper.Shared;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace WindowsHelper.Tests
+{
+    public class NotionServiceTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        [Fact]
+        public async Task DescribeCoursesDatabaseAsync_ShouldSucceed_InHappyPath()
+        {
+            #region Arrange
+
+            var notionSettings = TestHelper.GetFromAppSettings<NotionSettings>("Notion");
+
+            INotionService service = new NotionService(new NotionSettings
+            {
+                CoursesDatabaseId = notionSettings.CoursesDatabaseId,
+                Token = notionSettings.Token
+            });
+
+            #endregion
+
+            #region Act
+
+            var coursesDatabase = await service.DescribeCoursesDatabaseAsync();
+
+            #endregion
+
+            #region Assert
+
+            Assert.Equal("database", coursesDatabase?.Object);
+
+            #endregion
+        }
+    }
+}
