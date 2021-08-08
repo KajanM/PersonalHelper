39b556d9fc72e7d08d79e5a00309310992223efc
kajan <gmkajan@gmail.com>
2021-08-08 16:25:52 +0530

    feat: add extension method to serialize, deserialize yaml

---
+++ WindowsHelper.Tasks/Extensions/YamlExtensions.cs
@@ -0,0 +1,28 @@
+﻿using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace WindowsHelper.Tasks.Extensions
+{
+    public static class YamlExtensions
+    {
+        public static string SerializeToYaml<T>(this T @source) where T : class
+        {
+            var serializer = new SerializerBuilder()
+                .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .Build();
+
+            return serializer.Serialize(@source);
+        }
+
+        public static TOut DeserializeYaml<TOut>(this string yamlString, INamingConvention yamlNamingConvention = null)
+        {
+            yamlNamingConvention ??= CamelCaseNamingConvention.Instance;
+            
+            var deserializer = new DeserializerBuilder()
+                .WithNamingConvention(yamlNamingConvention)
+                .Build();
+            
+            return deserializer.Deserialize<TOut>(yamlString);
+        }
+    }
+}
