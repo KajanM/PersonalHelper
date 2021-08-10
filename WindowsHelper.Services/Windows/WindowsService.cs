3d6696098132e11b1cade2572cd3218b6a7bfec3
kajan <gmkajan@gmail.com>
2021-08-11 00:12:40 +0530

    refactor: allow specifying shutdown-in-minutes instead of hours

--- WindowsHelper.Services/Windows/WindowsService.cs
+++ WindowsHelper.Services/Windows/WindowsService.cs
@@ -4,9 +4,9 @@ namespace WindowsHelper.Services.Windows
 {
     public static class WindowsService
     {
-        public static void Shutdown(int hours = 0)
+        public static void Shutdown(int minutes = 0)
         {
-            Process.Start("shutdown", $"/s /t {hours * 3600}");
+            Process.Start("shutdown", $"/s /t {minutes * 60}");
         }
     }
 }
