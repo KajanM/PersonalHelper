48e24a1fcfe116a87ec703957b91be57c9324087
kajan <gmkajan@gmail.com>
2021-08-20 04:13:25 +0530

    feat: add extension method to truncate string

---
+++ WindowsHelper.Services/Extensions/StringExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace WindowsHelper.Services.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ReplaceInvalidChars(this string @this, string replaceByChar = "-")
+        {
+            return Regex
+                .Replace(Path.GetFileNameWithoutExtension(@this), @"[^A-Za-z0-9]+", replaceByChar)
+                .ToLower();
+        }
+        /// <summary>
+        /// Truncates string so that it is no longer than the specified number of characters.
+        /// </summary>
+        /// <param name="str">String to truncate.</param>
+        /// <param name="length">Maximum string length.</param>
+        /// <returns>Original string or a truncated one if the original was too long.</returns>
+        public static string Truncate(this string str, int length)
+        {
+            if (length < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(length), "Length must be >= 0");
+            }
+
+            if (str == null)
+            {
+                return null;
+            }
+
+            var maxLength = Math.Min(str.Length, length);
+            return str[..maxLength];
+        }
+    }
+}
