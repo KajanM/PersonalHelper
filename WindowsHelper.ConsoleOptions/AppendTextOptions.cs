bb979ecee4a4ce628c61e60030d74b02d253c2d1
kajan <gmkajan@gmail.com>
2021-08-28 08:13:05 +0530

    feat: change default pattern to include both jsx, tsx

--- WindowsHelper.ConsoleOptions/AppendTextOptions.cs
+++ WindowsHelper.ConsoleOptions/AppendTextOptions.cs
@@ -11,8 +11,8 @@ namespace WindowsHelper.ConsoleOptions
         [Option('p', "path", Required = false, HelpText = "Path to perform action. Defaults to the current directory")]
         public string Path { get; set; } = Environment.CurrentDirectory;
 
-        [Option(Required = false, HelpText = "Matching file name pattern. Defaults to App.tsx")]
-        public string Pattern { get; set; } = "App.tsx";
+        [Option(Required = false, HelpText = "Matching file name pattern. Defaults to App.*sx")]
+        public string Pattern { get; set; } = "App.*sx";
 
         [Option('t', "text", Required = false, HelpText = "Text to append. Defaults to 'export default App'")]
         public string AppendText { get; set; } = "export default App";
