25680a069b72b5631280c3d9b3ce8d58d2316f54
kajan <gmkajan@gmail.com>
2021-08-30 06:42:29 +0530

    refactor: allow adding more than 16 verbs

--- WindowsHelper.ConsoleApp/App.cs
+++ WindowsHelper.ConsoleApp/App.cs
@@ -14,49 +14,92 @@ namespace WindowsHelper.ConsoleApp
     {
         public static async Task StartAsync(string[] args)
         {
-            await Parser.Default.ParseArguments<AppendNumberToFilesOptions, ChangeSystemTimeOptions,
-                    MoveToParentDirectoryOptions,
-                    ReplaceInvalidCharsFromFileNameOptions,
-                    JoinMultipleVideosFfmpegOptions,
-                    UploadToYoutubeOptions,
-                    GenerateUploadMetaTemplateFileOptions,
-                    CompressToH265Options,
-                    ShutdownOptions,
-                    CrawlUdemyCouponsOptions,
-                    DownloadFromLibgenOptions,
-                    AddToIdmQueueOptions,
-                    RefreshGoogleTokenOptions,
-                    UpdateFreshCouponsRepoOptions,
-                    ExtractZipOptions,
-                    AppendTextOptions
-                >(args)
-                .MapResult(
-                    async (AppendNumberToFilesOptions opts) => new FileOrganizer(opts).PrependFileNamesWithNumber(),
-                    async (ChangeSystemTimeOptions opts) => await WindowsTimeService.ExecuteAsync(opts),
-                    async (MoveToParentDirectoryOptions opts) => new MoveToParentDirectory(opts).Move(),
-                    async (ReplaceInvalidCharsFromFileNameOptions opts) => ReplaceInvalidCharsFromFileName.Execute(opts),
-                    async (JoinMultipleVideosFfmpegOptions opts) => await new JoinMultipleVideosFfmpeg(opts).JoinAsync(),
-                    async (UploadToYoutubeOptions opts) => await new UploadToYoutube(opts,
-                        Program.AppSettings.YoutubeSettings, Program.AppSettings.NotionSettings).ExecuteAsync(),
-                    async (GenerateUploadMetaTemplateFileOptions opts) => await GenerateUploadMetaTemplateFile.ExecuteAsync(opts),
-                    async (CompressToH265Options opts) => await new CompressToH265(opts).ExecuteAsync(),
-                    async (ShutdownOptions opts) => WindowsService.Shutdown(opts.Minutes, opts.Hours, opts.Seconds),
-                    async (CrawlUdemyCouponsOptions opts) => await new CrawlUdemyCoupons().ExecuteAsync(),
-                    async (DownloadFromLibgenOptions opts) => await new DownloadFromLibgen().ExecuteAsync(opts),
-                    async (AddToIdmQueueOptions opts) => AddToIdmQueue.ExecuteAsync(opts),
-                    async (RefreshGoogleTokenOptions opts) => new RefreshGoogleTokens(opts, Program.AppSettings.YoutubeSettings).Execute(),
-                    async (UpdateFreshCouponsRepoOptions opts) => await UpdateFreshCouponsRepo.ExecuteAsync(opts),
-                    async (ExtractZipOptions opts) => ExtractZip.Execute(opts),
-                    async (AppendTextOptions opts) => await AppendText.ExecuteAsync(opts),
-                    HandleParseErrorAsync);
+            var types = new[]
+            {
+                typeof(AppendNumberToFilesOptions), typeof(ChangeSystemTimeOptions),
+                typeof(MoveToParentDirectoryOptions),
+                typeof(ReplaceInvalidCharsFromFileNameOptions),
+                typeof(JoinMultipleVideosFfmpegOptions),
+                typeof(UploadToYoutubeOptions),
+                typeof(GenerateUploadMetaTemplateFileOptions),
+                typeof(CompressToH265Options),
+                typeof(ShutdownOptions),
+                typeof(CrawlUdemyCouponsOptions),
+                typeof(DownloadFromLibgenOptions),
+                typeof(AddToIdmQueueOptions),
+                typeof(RefreshGoogleTokenOptions),
+                typeof(UpdateFreshCouponsRepoOptions),
+                typeof(ExtractZipOptions),
+                typeof(AppendTextOptions)
+            };
+            var result = Parser.Default.ParseArguments(args, types);
+            result.MapResult(async (object opts) => await RunAsync(opts),HandleParseErrorAsync);
+        }
+
+        static async Task<int> RunAsync(object options)
+        {
+            Log.Debug("Options received {Options}", options);
+
+            switch (options)
+            {
+                case AppendNumberToFilesOptions opts:
+                    new FileOrganizer(opts).PrependFileNamesWithNumber();
+                    return 0;
+                case ChangeSystemTimeOptions opts:
+                    await WindowsTimeService.ExecuteAsync(opts);
+                    return 0;
+                case MoveToParentDirectoryOptions opts:
+                    new MoveToParentDirectory(opts).Move();
+                    return 0;
+                case ReplaceInvalidCharsFromFileNameOptions opts:
+                    ReplaceInvalidCharsFromFileName.Execute(opts);
+                    return 0;
+                case JoinMultipleVideosFfmpegOptions opts:
+                    await new JoinMultipleVideosFfmpeg(opts).JoinAsync();
+                    return 0;
+                case UploadToYoutubeOptions opts:
+                    await new UploadToYoutube(opts, Program.AppSettings.YoutubeSettings,
+                        Program.AppSettings.NotionSettings).ExecuteAsync();
+                    return 0;
+                case GenerateUploadMetaTemplateFileOptions opts:
+                    await GenerateUploadMetaTemplateFile.ExecuteAsync(opts);
+                    return 0;
+                case CompressToH265Options opts:
+                    await new CompressToH265(opts).ExecuteAsync();
+                    return 0;
+                case ShutdownOptions opts:
+                    WindowsService.Shutdown(opts.Minutes, opts.Hours, opts.Seconds);
+                    return 0;
+                case CrawlUdemyCouponsOptions opts:
+                    await new CrawlUdemyCoupons().ExecuteAsync();
+                    return 0;
+                case DownloadFromLibgenOptions opts:
+                    await new DownloadFromLibgen().ExecuteAsync(opts);
+                    return 0;
+                case AddToIdmQueueOptions opts:
+                    AddToIdmQueue.ExecuteAsync(opts);
+                    return 0;
+                case RefreshGoogleTokenOptions opts:
+                    new RefreshGoogleTokens(opts, Program.AppSettings.YoutubeSettings).Execute();
+                    return 0;
+                case UpdateFreshCouponsRepoOptions opts:
+                    await UpdateFreshCouponsRepo.ExecuteAsync(opts);
+                    return 0;
+                case ExtractZipOptions opts:
+                    ExtractZip.Execute(opts);
+                    return 0;
+                case AppendTextOptions opts:
+                    await AppendText.ExecuteAsync(opts);
+                    return 0;
+                default:
+                    return -1;
+            }
         }
 
         static async Task<int> HandleParseErrorAsync(IEnumerable<Error> errs)
         {
             Log.Error(string.Join(Environment.NewLine, errs));
-            Console.ReadLine();
-
-            return -1;
+            return 1;
         }
     }
 }
