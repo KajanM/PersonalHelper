6af21dac8ad4c6b0effbe6db52959752f2c78ef3
kajan <gmkajan@gmail.com>
2021-10-09 15:07:29 +0530

    feat: wrap main execution logic inside `try` `catch`

--- WindowsHelper.ConsoleApp/App.cs
+++ WindowsHelper.ConsoleApp/App.cs
@@ -44,71 +44,79 @@ namespace WindowsHelper.ConsoleApp
         {
             Log.Debug("Options received {Options}", options);
 
-            switch (options)
+            try
             {
-                case AppendNumberToFilesOptions opts:
-                    new FileOrganizer(opts).PrependFileNamesWithNumber();
-                    return 0;
-                case ChangeSystemTimeOptions opts:
-                    await WindowsTimeService.ExecuteAsync(opts);
-                    return 0;
-                case MoveToParentDirectoryOptions opts:
-                    new MoveToParentDirectory(opts).Move();
-                    return 0;
-                case ReplaceInvalidCharsFromFileNameOptions opts:
-                    ReplaceInvalidCharsFromFileName.Execute(opts);
-                    return 0;
-                case JoinMultipleVideosFfmpegOptions opts:
-                    await JoinMultipleVideosFfmpeg.JoinAsync(opts);
-                    return 0;
-                case UploadToYoutubeOptions opts:
-                    await new UploadToYoutube(opts, Program.AppSettings.GoogleSettings,
-                        Program.AppSettings.NotionSettings).ExecuteAsync();
-                    return 0;
-                case GenerateUploadMetaTemplateFileOptions opts:
-                    await GenerateUploadMetaTemplateFile.ExecuteAsync(opts);
-                    return 0;
-                case CompressToH265Options opts:
-                    await new CompressToH265(opts).ExecuteAsync();
-                    return 0;
-                case ShutdownOptions opts:
-                    WindowsService.Shutdown(opts.Minutes, opts.Hours, opts.Seconds);
-                    return 0;
-                case CrawlUdemyCouponsOptions opts:
-                    new CrawlUdemyCoupons().ExecuteAsync().Wait();
-                    return 0;
-                case DownloadFromLibgenOptions opts:
-                    await new DownloadFromLibgen().ExecuteAsync(opts);
-                    return 0;
-                case AddToIdmQueueOptions opts:
-                    AddToIdmQueue.ExecuteAsync(opts);
-                    return 0;
-                case RefreshGoogleTokenOptions opts:
-                    new RefreshGoogleTokens(opts, Program.AppSettings.GoogleSettings).Execute();
-                    return 0;
-                case UpdateFreshCouponsRepoOptions opts:
-                    await UpdateFreshCouponsRepo.ExecuteAsync(opts);
-                    return 0;
-                case ExtractZipOptions opts:
-                    ExtractZip.Execute(opts);
-                    return 0;
-                case AppendTextOptions opts:
-                    await AppendText.ExecuteAsync(opts);
-                    return 0;
-                case AddWatermarkToVideoOptions opts:
-                    AddWatermarkToVideo.Execute(opts);
-                    return 0;
-                case RemoveStringFromNameOptions opts:
-                    RemoveStringFromName.Execute(opts);
-                    return 0;
-                case DownloadFromGDriveOptions opts:
-                    await new DownloadFromGDrive(opts, Program.AppSettings.GoogleSettings).ExecuteAsync();
-                    return 0;
-                case JoinVideosInInnerDirectoryAndMoveToParentDirectoryOptions opts:
-                    JoinVideosInInnerDirectoryAndMoveToParentDirectory.Execute(opts);
-                    return 0;
-                default:
-                    return -1;
+                switch (options)
+                {
+                    case AppendNumberToFilesOptions opts:
+                        new FileOrganizer(opts).PrependFileNamesWithNumber();
+                        return 0;
+                    case ChangeSystemTimeOptions opts:
+                        await WindowsTimeService.ExecuteAsync(opts);
+                        return 0;
+                    case MoveToParentDirectoryOptions opts:
+                        new MoveToParentDirectory(opts).Move();
+                        return 0;
+                    case ReplaceInvalidCharsFromFileNameOptions opts:
+                        ReplaceInvalidCharsFromFileName.Execute(opts);
+                        return 0;
+                    case JoinMultipleVideosFfmpegOptions opts:
+                        await JoinMultipleVideosFfmpeg.JoinAsync(opts);
+                        return 0;
+                    case UploadToYoutubeOptions opts:
+                        await new UploadToYoutube(opts, Program.AppSettings.GoogleSettings,
+                            Program.AppSettings.NotionSettings).ExecuteAsync();
+                        return 0;
+                    case GenerateUploadMetaTemplateFileOptions opts:
+                        await GenerateUploadMetaTemplateFile.ExecuteAsync(opts);
+                        return 0;
+                    case CompressToH265Options opts:
+                        await new CompressToH265(opts).ExecuteAsync();
+                        return 0;
+                    case ShutdownOptions opts:
+                        WindowsService.Shutdown(opts.Minutes, opts.Hours, opts.Seconds);
+                        return 0;
+                    case CrawlUdemyCouponsOptions opts:
+                        new CrawlUdemyCoupons().ExecuteAsync().Wait();
+                        return 0;
+                    case DownloadFromLibgenOptions opts:
+                        await new DownloadFromLibgen().ExecuteAsync(opts);
+                        return 0;
+                    case AddToIdmQueueOptions opts:
+                        AddToIdmQueue.ExecuteAsync(opts);
+                        return 0;
+                    case RefreshGoogleTokenOptions opts:
+                        new RefreshGoogleTokens(opts, Program.AppSettings.GoogleSettings).Execute();
+                        return 0;
+                    case UpdateFreshCouponsRepoOptions opts:
+                        await UpdateFreshCouponsRepo.ExecuteAsync(opts);
+                        return 0;
+                    case ExtractZipOptions opts:
+                        ExtractZip.Execute(opts);
+                        return 0;
+                    case AppendTextOptions opts:
+                        await AppendText.ExecuteAsync(opts);
+                        return 0;
+                    case AddWatermarkToVideoOptions opts:
+                        AddWatermarkToVideo.Execute(opts);
+                        return 0;
+                    case RemoveStringFromNameOptions opts:
+                        RemoveStringFromName.Execute(opts);
+                        return 0;
+                    case DownloadFromGDriveOptions opts:
+                        await new DownloadFromGDrive(opts, Program.AppSettings.GoogleSettings).ExecuteAsync();
+                        return 0;
+                    case JoinVideosInInnerDirectoryAndMoveToParentDirectoryOptions opts:
+                        JoinVideosInInnerDirectoryAndMoveToParentDirectory.Execute(opts);
+                        return 0;
+                    default:
+                        return -1;
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Error(e, "An error occured while processing");
+                return -1;
             }
         }
 
